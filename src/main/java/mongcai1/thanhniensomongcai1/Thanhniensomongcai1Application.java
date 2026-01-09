c73087b5f782c3b10117291b075a847082d11c69
Lê Gia Bảo <legiabao401@gmail.com>
2026-01-09 14:22:56 +0700

    Khoi tao

---
+++ src/main/java/mongcai1/thanhniensomongcai1/Thanhniensomongcai1Application.java
@@ -0,0 +1,13 @@
+package mongcai1.thanhniensomongcai1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Thanhniensomongcai1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Thanhniensomongcai1Application.class, args);
+    }
+
+}
---
+++ src/test/java/mongcai1/thanhniensomongcai1/Thanhniensomongcai1ApplicationTests.java
@@ -0,0 +1,13 @@
+package mongcai1.thanhniensomongcai1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Thanhniensomongcai1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
